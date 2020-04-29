03fa4aee5607448808116cb5ebc7107f4940e186
Shimpi <maheshratilal.shimpi@quintiles.com>
2020-04-29 17:12:52 +0530

    ch

--- wp-content/themes/myblog/header.php
+++ wp-content/themes/myblog/header.php
@@ -62,7 +62,7 @@
 						'link_after'      => '',
 						'items_wrap'      => '<ul class="navbar-nav flex-column text-left">%3$s</ul>',
 						'item_spacing'    => 'preserve',
-						'depth'           => 0,
+						'depth'           => 3,
 						'walker'          => '',
 						'theme_location'  => 'primary',
 					);
