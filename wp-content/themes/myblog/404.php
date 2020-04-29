1aca9d3d47660c204904e1506480168895d367c8
Shimpi <maheshratilal.shimpi@quintiles.com>
2020-04-29 12:51:19 +0530

    function.php added

---
+++ wp-content/themes/myblog/404.php
@@ -0,0 +1,22 @@
+<?php
+get_header();
+?>
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main">
+
+			<div class="error-404 not-found">
+				<header class="page-header">
+					<h1 class="page-title">404 Error</h1>
+				</header><!-- .page-header -->
+
+				<div class="page-content">
+					<p>Looks like something is wrong.</p>
+				</div><!-- .page-content -->
+			</div><!-- .error-404 -->
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php
+get_footer();
