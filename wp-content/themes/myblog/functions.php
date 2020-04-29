f47d14f077814e9ad40f1ea36aee7f1d322366e6
Shimpi <maheshratilal.shimpi@quintiles.com>
2020-04-29 18:04:41 +0530

    css

--- wp-content/themes/myblog/functions.php
+++ wp-content/themes/myblog/functions.php
@@ -9,12 +9,19 @@
 		);
 	}
 	add_action( 'init', 'register_menu' );
+//---------------- hooks ---------------------------
 
-
+//---------------- filter ---------------------------
 	function add_menu_link_class($atts, $item, $args){
 		$atts['class'] = 'nav-link';
 		// echo 'Item :'.$item;
 		return $atts;
 	}
 	add_filter('nav_menu_link_attributes', 'add_menu_link_class', 1, 3);
+
+	function add_classes_on_li($classes, $item, $args) {
+		$classes[] = 'nav-item';
+		return $classes;
+	  }
+	  add_filter('nav_menu_css_class','add_classes_on_li',1,3);
 ?>
